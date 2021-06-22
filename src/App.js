297dc1f7c94756dd5770e6e30c8e91dd3461fc16
Shohanur Rahman <srshohan026@gmail.com>
2021-06-22 23:46:32 +0600

    Setup Project folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <codes>src/App.js</codes> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>sdfa sd</h2>
     </div>
   );
 }
